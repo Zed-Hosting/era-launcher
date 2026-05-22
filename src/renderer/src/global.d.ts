67c61d5e0f0dfedc56faf4be7ef5be3f0d3e5801
Zed-Hosting <ops@zed-hosting.local>
2026-05-22 00:50:15 -0500

    v0.1.44: add image module type declarations to global.d.ts

--- src/renderer/src/global.d.ts
+++ src/renderer/src/global.d.ts
@@ -6,4 +6,12 @@ declare global {
   }
 }
 
+// Image asset imports
+declare module '*.jpg' { const src: string; export default src }
+declare module '*.jpeg' { const src: string; export default src }
+declare module '*.png' { const src: string; export default src }
+declare module '*.webp' { const src: string; export default src }
+declare module '*.gif' { const src: string; export default src }
+declare module '*.svg' { const src: string; export default src }
+
 export {}
